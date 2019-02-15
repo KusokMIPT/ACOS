3750536868ec3e24ec245e6355e529f5c04ac7f5
KusochekDobra <podkidyshev.as@phystech.edu>
2019-02-16 01:59:02 +0400

    Не понял как работает realloc
    Segmentetion fault

--- 1.c
+++ 1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+
 /*
 void split(char * str, size_t size, char* res){
 
@@ -28,82 +29,88 @@ int compare_string (const void * str1, const void * str2) {
     return strcmp(s1, s2);
 }
 
-void out_str_array(char **str, int size){
-    for(int i = 0; i < size; ++i){
-    printf("%s\n", str[i]);
+void out_str_array(char **str, int size) {
+    for(int i = 0; i < size; ++i) {
+        if (i!= size -1)printf("%s\n", str[i]);
+
+        else printf("%s", str[i]);
     }
 }
 
-int main(){
-    //freopen("input.txt","r", stdin);
-	char c;
-	char *str = NULL;
-	str = realloc(str,1);
-	unsigned int size = 0;
+int main() {
+    freopen("input.txt","r", stdin);
+    char c;
+    char *str = NULL;
+    str = realloc(str,1);
+    unsigned int size = 0;
 
-	///String's reading
+    ///String's reading
 
-	while ((c = getchar()) != EOF){
-	str[size++] = c;
-	str = realloc(str, size+1);
-	}
-	str[size] = '\0';
+    while ((c = getchar()) != EOF) {
+        str[size++] = c;
+        str = realloc(str, size+1);
+    }
+    str[size] = '\0';
 
-	///Processing of String's
+    ///Processing of String's
 
-	char **strs = NULL;
+    char **strs = NULL;
     strs = realloc(strs, 1);
-	unsigned int cur_size = 1, num_of_str = 0;
+    unsigned int cur_size = 1, num_of_str = 0;
 
-	for(int i = 0 ; i < size; ++i){
+    int i = 0;
+    while(isspace(str[i])) {
+        ++i;
+    }
 
-	if(isspace(str[i])){
+    for(; i < size; ++i) {
 
-        int zashel = 0;
-        while(isspace(str[i]) && i < size){
-            ++i;
-            zashel = 1;
+        if(isspace(str[i])) {
+
+            int zashel = 0;
+            while(isspace(str[i]) && i < size) {
+                ++i;
+                zashel = 1;
+            }
+
+            strs[num_of_str][cur_size] = '\"';
+            cur_size++;
+            strs[num_of_str] = realloc(strs[num_of_str], cur_size + 1);
+            strs[num_of_str][cur_size] = '\0';
+
+            //Check str Ending
+            if(i == size) break;
+
+            strs = realloc(strs, num_of_str + 1);\
+            ++num_of_str;
+            cur_size = 1
+		    
+            if(zashel) --i;
+            continue;
         }
 
-        strs[num_of_str][cur_size] = '\"';
-        cur_size++;
         strs[num_of_str] = realloc(strs[num_of_str], cur_size + 1);
-        strs[num_of_str][cur_size] = '\0';
-
-        //Check str Ending
-        if(i == size) break;
-
-        strs = realloc(strs, num_of_str + 1);
-        ++num_of_str;
-        cur_size = 1;
-        if(zashel) --i;
-        //printf("%s\n", strs[num_of_str - 1]);
-        continue;
-	}
-
-	strs[num_of_str] = realloc(strs[num_of_str], cur_size+1);
-    strs[num_of_str][0] = '\"';
-	strs[num_of_str][cur_size] =  str[i];
-    ++cur_size;
-	}
+        strs[num_of_str][0] = '\"';
+        strs[num_of_str][cur_size] =  str[i];
+        ++cur_size;
+    }
     if(num_of_str != 0) ++num_of_str;
-	//if(str[size-1] == '\n') --num_of_str;
-
-	//out_str_array(strs, num_of_str);
-
-	///Sort list of string's
-	qsort(strs, num_of_str, sizeof(char *), compare_string);
+    else {
+        if(cur_size != 0) ++num_of_str;
+    }
+    ///Sort list of string's
+    qsort(strs, num_of_str, sizeof(char *), compare_string);
 
     out_str_array(strs, num_of_str);
 
+    free(str);
 
-	free(str);
-
-	for(int i = 0; i < num_of_str; ++i){
+    for(int i = 0; i < num_of_str; ++i) {
         free(strs[i]);
-	}
-	free(strs);
+    }
+    free(strs);
 
 
-	return 0;
+    return 0;
 }
+
