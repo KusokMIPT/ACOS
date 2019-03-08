8195f3a325b9e6f0f848a55be85e975cd4bc03a9
KusokMIPT <podkidyshev.as@phystech.edu>
2019-03-08 12:39:41 +0400

    OK

--- wc.c
+++ wc.c
@@ -9,70 +9,55 @@ typedef unsigned int uint;
 uint lines_nums = 0, word_num = 0, symb_num = 0;
 char* CurfileName = "";
 
-void smartFileReading(FILE **f) {
+int smartFileReading(FILE **f) {
     *f = fopen(CurfileName, "r");
     if(*f == NULL) {
         perror("Bad File Name");
+        return 0;
     }
+    return 1;
 }
 
 void OneFileProcessoring(FILE* file) {
-    char str[10000];
     int flag = 0;
 
-    struct stat spec_struct;
-    while((fgets(str, sizeof(str), file)) != NULL) {
-        for(int i = 0; i < strlen(str); ++i) {
-            if(isspace(str[i]) || flag == 2) {
-                flag = 0;
-                if(str[i] == '\n') {
-                    ++lines_nums;
-                }
-                continue;
+    int c;
+    while((c = fgetc(file)) != EOF){
+        ++symb_num;
+        if(isspace(c)){
+            if(c == '\n'){
+                ++lines_nums;
             }
-
+            if(flag) {
             ++word_num;
-            flag = 1;
-            while(i + 1 < strlen(str) && !isspace(str[i + 1])) {
-                ++i;
             }
-        }
-        if(flag == 1){
-            flag = 2;
-        }
-        else{
             flag = 0;
+
+            continue;
         }
 
-        int a = 1;
+        flag = 1;
     }
-
-    //умное определение количество символов
-    if (fstat(fileno(file), &spec_struct) != 0) {
-        exit(EXIT_FAILURE);
-    } else {
-        symb_num = spec_struct.st_size;
+    if(flag){
+        word_num++;
     }
-    printf("%d %d %d", lines_nums, word_num, symb_num);
+    printf("%d %d %d %s\n", lines_nums, word_num, symb_num, CurfileName);
     lines_nums = 0, word_num = 0, symb_num = 0;
-    printf(" %s\n", CurfileName);
 }
 
 int main(int argc, char** argv) {
-    argc = 2;
-    argv[1] = "input.txt";
     if(argc > 1) {
         for(int i = 1; i < argc; ++i) {
             FILE * file;
             CurfileName = argv[i];
-            smartFileReading(&file);
-            OneFileProcessoring(file);
-            fclose(file);
+            if(smartFileReading(&file)){
+                OneFileProcessoring(file);
+                fclose(file);
+            }
         }
     }
     else {
-        FILE* file = stdin;
-        OneFileProcessoring(file);
+        OneFileProcessoring(stdin);
     }
 
     return 0;
